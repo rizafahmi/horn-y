f63112a45c38af6fad60215ca5a26f7969733e56
Riza Fahmi <rizafahmi@gmail.com>
2014-12-22 14:15:53 +0700

    Play terompet sound

--- Horn-y/ViewController.m
+++ Horn-y/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController ()
+@interface ViewController () {
+    AVAudioPlayer *_audioPlayer;
+}
 
 @end
 
@@ -17,11 +20,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *path = [NSString stringWithFormat:@"%@/terompet.wav", [[NSBundle mainBundle] resourcePath]];
+    NSURL *soundURL = [NSURL fileURLWithPath:path];
+    
+    _audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:soundURL error:nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)tombolDipencet {
+    NSLog(@"Dipencet");
+    [_audioPlayer play];
 }
 
 @end
